74ea74639dbf4da171d503b7fbe51eb3a4e49491
Ben Lucas <124517145+retcon1@users.noreply.github.com>
2024-03-07 17:27:33 +0000

    FEAT: adds check to see if username is taken

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,10 +1,14 @@
 import User from "../models/user.model.js";
 
 export const registerUser = async (req, res) => {
+  const { username, password } = req.body;
+  const existingUser = await User.findOne({ username });
+  if (existingUser) return res.status(400).json({ message: "Username Taken" });
+
   try {
     const user = new User({
-      username: req.body.username,
-      password: req.body.password
+      username: username,
+      password: password,
     });
     const newUser = await user.save();
     res.status(201).json(newUser);
