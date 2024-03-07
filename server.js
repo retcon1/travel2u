0847a1bfe7322115be90ba076d522db44d76f13e
Ben Lucas <124517145+retcon1@users.noreply.github.com>
2024-03-07 16:35:37 +0000

    FEAT: adds and initiates server

---
+++ server.js
@@ -0,0 +1,41 @@
+import { config } from "dotenv";
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+
+config();
+
+// Original method of displaying connection
+// mongoose
+//   .connect(process.env.MONGO_URI)
+//   .then(() => {
+//     console.log("Connected to MongoDB!");
+//   })
+//   .catch((err) => {
+//     console.error(`Connection error`, err);
+//     process.exit();
+//   });
+
+// This method utilises mongoose functionality
+mongoose.connect(process.env.MONGO_URI);
+
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to database"));
+
+const app = express();
+
+app.use(cors());
+
+app.use(bodyParser.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+const port = process.env.PORT ?? 8080;
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
+
+export default app;
